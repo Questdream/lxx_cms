b783ab870bf0ed3252ab171439f267d8aa7205c1
lxx <lxx249@163.com>
2021-05-07 21:55:15 +0800

    完成resource和video组件的交互以及最后的所有效果图

--- client/src/views/Resource.vue
+++ client/src/views/Resource.vue
@@ -4,7 +4,7 @@
         <el-button type="danger" round @click="creatResource">添加网盘资源</el-button>
         <!-- 添加表格 -->
         <el-table
-        :data="tableData"
+        :data="resourceList"
         border>
             <el-table-column
                 align="center"
@@ -27,47 +27,111 @@
             <el-table-column
                 label="操作"
                 width="200">
+                <template slot-scope="scope">
+                    <el-button @click="updateResource(scope.row)" type="text" size="small">编辑</el-button>
+                    <el-button @click="deleteResource(scope.row.id)" type="text" size="small">删除</el-button>
+                </template>
             </el-table-column>
         </el-table>
         <!-- 添加百度资源的对话框 -->
         <el-dialog title="添加资源" :visible.sync="dialogFormVisible" width="30%">
-            <el-form :model="form">
-                <el-form-item label="资源名称" :label-width="formLabelWidth">
-                <el-input v-model="form.title" autocomplete="off"></el-input>
+            <el-form>
+                <el-form-item label="资源名称">
+                <el-input v-model="title" autocomplete="off"></el-input>
                 </el-form-item>
-                <el-form-item label="资源地址" :label-width="formLabelWidth">
-                <el-input v-model="form.url" autocomplete="off"></el-input>
+                <el-form-item label="资源地址">
+                <el-input v-model="url" autocomplete="off"></el-input>
                 </el-form-item>
-                <el-form-item label="提取码" :label-width="formLabelWidth">
-                <el-input v-model="form.code" autocomplete="off"></el-input>
+                <el-form-item label="提取码">
+                <el-input v-model="code" autocomplete="off"></el-input>
                 </el-form-item>
                 
             </el-form>
             <div slot="footer" class="dialog-footer">
                 <el-button @click="dialogFormVisible = false">取 消</el-button>
-                <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+                <el-button type="primary" @click="save">确 定</el-button>
             </div>
         </el-dialog>
     </div>
 </template>
 
 <script>
+import request from '../utils/request';
 export default {
     data(){
         return {
-            tableData:[],
-            formLabelWidth:"",
+            resourceList:[],
             dialogFormVisible:false,
-            form: {
-                title:"",
-                url:"",
-                code:""
-            }
+            isEdit: false,
+            updateId: 0,
+            title:"",
+            url:"",
+            code:""
+            
         }
     },
+    created(){
+        this.getResourceList();
+    },
     methods: {
+        getResourceList(){
+            request.get(`/resource`).then((res) => {
+                this.resourceList = res.data.data;
+            })
+        },
         creatResource(){
-            this.dialogFormVisible = true
+            this.dialogFormVisible = true;
+            this.title = "";
+            this.isEdit = false;
+        },
+        updateResource(row){
+            this.dialogFormVisible = true;
+            this.title = row.title;
+            this.updateId = row.id;
+            this.url = row.url;
+            this.code = code;
+            this.isEdit = true;
+        },
+        deleteResource(id){
+            this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => {
+                request.delete(`/resource/${id}`).then(() => {
+                    this.$message({
+                        type: 'success',
+                        message: '删除成功!'
+                    });
+                    this.getResourceList();
+                })
+            }).catch(() => {
+                this.$message({
+                    type: 'info',
+                    message: '已取消删除'
+                });          
+            });
+        },
+        save(){
+            if(this.isEdit){
+                request.put(`/resource/${this.updateId}`, {
+                    title: this.title,
+                    code: this.code,
+                    url: this.url,
+                }).then(() => {
+                    this.getResourceList();
+                    this.dialogFormVisible = false;
+                })
+            }else{
+                request.post(`/resource`, {
+                    title: this.title,
+                    code: this.code,
+                    url: this.url,
+                }).then(() => {
+                    this.getResourceList();
+                    this.dialogFormVisible = false;
+                })
+            }
         }
     }
 }
--- client/src/views/Video.vue
+++ client/src/views/Video.vue
@@ -3,26 +3,44 @@
         <el-button type="danger" round @click="createVideo">添加视频</el-button>
         <!-- 添加表格 -->
         <el-table
-        :data="tableData"
+        :data="videoList"
         border>
             <el-table-column
                 align="center"
                 label="序号"
                 width="100">
             </el-table-column>
+
             <el-table-column
-                label="封面"
-                >
+                label="封面">
+                <template slot-scope="scope">
+                    <img class="pic" :src="scope.row.img" alt />
+                </template>
             </el-table-column>
+
             <el-table-column
-                label="标题"
-                >
+                label="姓名">
             </el-table-column>
+            
             <el-table-column
-                label="操作"
-                >
+                label="详情">
+                <template slot-scope="scope">
+                    <el-button @click="getDetail(scope.row.iframe_url)" type="text" size="small">浏览视频</el-button>
+                </template>
+            </el-table-column>
+
+            <el-table-column
+                label="操作">
+                <template slot-scope="scope">
+                    <el-button @click="updateVideo(scope.row)" type="text" size="small">编辑</el-button>
+                    <el-button @click="deleteVideo(scope.row.id)" type="text" size="small">删除</el-button>
+                </template>
             </el-table-column>
         </el-table>
+        <!-- 添加视频播放的小窗口 -->
+        <el-dialog title="添加" :visible.sync="videoIsShow" width="800px">
+            <div class="test" v-html="video_url"></div>
+        </el-dialog>
 
         <!-- 添加视频的对话框 -->
         <el-dialog title="添加视频" :visible.sync="dialogFormVisible" width="30%">
@@ -31,44 +49,124 @@
                 <el-upload
                 class="upload-demo"
                 drag
-                action="https://jsonplaceholder.typicode.com/posts/"
+                :action="getUpUrl()"
                 multiple>
                 <i class="el-icon-upload"></i>
                 <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
                 <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
                 </el-upload>
             </div>
-            <el-form :model="form">
-                <el-form-item label="视频标题" :label-width="formLabelWidth">
-                <el-input v-model="form.title" autocomplete="off"></el-input>
+            <el-form>
+                <el-form-item label="视频标题">
+                <el-input v-model="title" autocomplete="off"></el-input>
                 </el-form-item>
-                <el-form-item label="视频地址" :label-width="formLabelWidth">
-                <el-input v-model="form.url" autocomplete="off"></el-input>
+                <el-form-item label="视频地址">
+                <el-input v-model="iframe_url" autocomplete="off"></el-input>
                 </el-form-item>
                 
             </el-form>
             <div slot="footer" class="dialog-footer">
                 <el-button @click="dialogFormVisible = false">取 消</el-button>
-                <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+                <el-button type="primary" @click="save">确 定</el-button>
             </div>
         </el-dialog>
     </div>
 </template>
 
 <script>
+import request from "../utils/request"
 export default {
     data(){
         return {
             dialogFormVisible:false,
-            form: {
-                title:"",
-                url:""
-            }
+            videoList: [],
+            iframe_url: "",
+            videoIsShow: false,
+            title: "",
+            isEdit: false,
+            updateId: 0,
+            img: "",
+            video_url: ""
         }
     },
+    created(){
+        this.getVideoList();
+    },
     methods: {
+        // 从后台获取页面展示情况
+        getVideoList(){
+            request.get(`/video`).then((res) => {
+                this.videoList = res.data.data;
+            })
+        },
+        // 上传图片的地址
+        getUpUrl(){
+            return process.env.VUE_APP_UPLOAD_API;
+        },
+        // 添加新的视频
         createVideo(){
-            this.dialogFormVisible = true
+            // 调出对话框来添加视频，将变量清空
+            this.dialogFormVisible = true;
+            this.title = "";
+            this.iframe_url = "";
+            this.isEdit = false;
+        },
+        // 获得视频观看窗口
+        getDetail(iframe_url){
+            this.videoIsShow = true;
+            this.video_url = iframe_url;
+        },
+        // 编辑视频情况
+        updateVideo(row){
+            this.dialogFormVisible = true;
+            this.title = row.title;
+            this.updateId = row.id;
+            this.iframe_url = row.iframe_url;
+            this.img = row.img;
+            this.isEdit = true;
+        },
+        // 删除视频
+        deleteVideo(id){
+            this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => {
+                request.delete(`/video/${id}`).then(() => {
+                    this.$message({
+                        type: 'success',
+                        message: '删除成功!'
+                    });
+                    this.getVideoList();
+                })
+            }).catch(() => {
+                this.$message({
+                    type: 'info',
+                    message: '已取消删除'
+                });          
+            });
+        },
+        // 保存修改或者新建
+        save(){
+            if(this.isEdit){
+                request.put(`/video/${this.updateId}`, {
+                    title: this.title,
+                    img: this.img,
+                    iframe_url: this.iframe_url,
+                }).then(() => {
+                    this.getVideoList();
+                    this.dialogFormVisible = false;
+                })
+            }else{
+                request.post(`/video`, {
+                    title: this.title,
+                    img: this.img,
+                    iframe_url: this.iframe_url,
+                }).then(() => {
+                    this.getVideoList();
+                    this.dialogFormVisible = false;
+                })
+            }
         }
     }
 }
